503c6f6afe2c428a2e3037351ed2798bab4de7e5
Bia Pinheiro <anabeatrizdp2@gmail.com>
2026-02-15 18:17:30 -0300

    Convertento str para int

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(input('Escolha uma opção: '))
+# opcao_escolhida = int(opcao_escolhida)
 print(f'Você escolheu a opção {opcao_escolhida}') #f-string (ou Formatted String Literal)
 
 if opcao_escolhida == 1:
@@ -15,6 +16,7 @@ elif opcao_escolhida == 3:
     print('Ativar restaurante')
 else:
     print('Encerrandoo programa')
+
 
 
 
