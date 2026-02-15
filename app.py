d794cedc4ac795937b5b1d96c26314baead43f20
Bia Pinheiro <anabeatrizdp2@gmail.com>
2026-02-15 16:32:04 -0300

    Iniciando projeto - Python

---
+++ app.py
@@ -0,0 +1,21 @@
+print('Sabor Express\n') #shift + seta para baixo -> multiplica | \n -> pula uma linha
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}') #f-string (ou Formatted String Literal)
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurante')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    print('Encerrandoo programa')
+
+
+
+
